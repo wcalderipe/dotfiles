62ec316a53d980eaa93e13e0c51583f890d94561
William Calderipe <wcalder@thoughtworks.com>
2018-05-07 10:36:32 -0300

    Add vim Plug plugin manager

--- .shell/macos-vim.sh
+++ .shell/macos-vim.sh
@@ -3,3 +3,9 @@
 pip3 install neovim
 
 ln -s $HOME/.vim $HOME/.config/nvim
+
+# Add vim-plug to autoload
+if [[ ! -f "$HOME/.vim/autoload/plug.vim" ]]; then
+  curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
