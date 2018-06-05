7e220081ebb2bd458025c9df3b2a8dfcc22783cc
William Calderipe <wcalderipe@gmail.com>
2018-06-05 01:35:53 -0400

    Add instructions to install nvm

--- .shell/macos-setup.sh
+++ .shell/macos-setup.sh
@@ -11,10 +11,15 @@ fi
 echo "Installing zprezto..."
 git clone --recursive https://github.com/sorin-ionescu/prezto.git $HOME/.zprezto
 
-# Add vim-plug to autoload
 if [[ ! -f "$HOME/.vim/autoload/plug.vim" ]]; then
+  echo "Installing vim-plug"
   curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+if [[ ! -d ~/.nvm ]]; then
+  echo "Installing Node Version Manager (NVM)..."
+  git clone https://github.com/creationix/nvm.git $HOME/.nvm
 fi
 
 $HOME/.shell/macos-preferences.sh
--- .zshrc
+++ .zshrc
@@ -5,6 +5,8 @@ source ~/.zprezto/init.zsh
 [[ -f ~/.shell/variables ]] && source ~/.shell/variables
 [[ -f ~/.shell/aliases ]] && source ~/.shell/aliases
 [[ -f ~/private-variables ]] && source ~/private-variables
+[[ -f ~/.nvm/nvm.sh ]] && source ~/.nvm/nvm.sh
 
 # Enable Ctrl-R for navigating through the history.
 bindkey "^R" history-incremental-search-backward
+
