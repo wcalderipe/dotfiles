a0c27724f289512619e3847063ab1cda068e6eed
William Calderipe <wcalderipe@gmail.com>
2019-02-19 20:18:57 +1100

    Update clean.sh script

--- .shell/clean.sh
+++ .shell/clean.sh
@@ -1,36 +1,21 @@
 #! /bin/bash
 
 echo "~> Removing dotfiles..."
-rm -f \
-  .gitconfig \
-  .tmux.conf \
-  .vimrc \
-  .zpreztorc \
-  .zshrc \
-  README.md
-
-echo "~> Removing tmux..."
-rm -rf ~/.tmux/
-
-echo "~> Removing scripts..."
-rm -rf ~/.shell/
-
-echo "~> Removing zprezto..."
-rm -rf ~/.zprezto/
-
-echo "~> Removing iTerm configurations..."
-rm -rf ~/.iterm/
-
-echo "~> Removing vim configurations..."
-rm -rf ~/.vim/
-
-echo "~> Removing vscode configurations..."
-rm -rf ~/.vscode/extensions.txt ~/.vscode/settings.json
-
-echo "~> Removing .gitignore..."
-rm -rf ~/.gitignore
-
-echo "~> Removing config manager at ~/.my-conf..."
-rm -rf ~/.my-conf/
+rm -rf \
+  ~/.gitconfig \
+  ~/.gitignore \
+  ~/.tmux.conf \
+  ~/.vimrc \
+  ~/.zpreztorc \
+  ~/.zshrc \
+  ~/.tmux \
+  ~/.shell \
+  ~/.zprezto \
+  ~/.iterm \
+  ~/.vim \
+  ~/.emacs.d \
+  ~/.lein \
+  ~/.vscode/extensions.txt \
+  ~/.vscode/settings.json
 
 echo "~> DONE!"
