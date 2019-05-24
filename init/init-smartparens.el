81b3fb259bf7904c5d1732eb802f05b2230b9fe5
ilmotta <46027+ilmotta@users.noreply.github.com>
2019-05-24 12:47:00 -0300

    Improve insertion of single quotes and backticks in Lisp modes

--- init/init-smartparens.el
+++ init/init-smartparens.el
@@ -3,3 +3,9 @@
 (add-hook 'clojurescript-mode-hook 'smartparens-mode)
 
 (smartparens-global-strict-mode 1)
+
+;; Do not generate a pair of single quotes or backticks unless the
+;; point is inside comments/strings.
+(let ((modes (list 'emacs-lisp-mode 'clojure-mode 'clojurescript-mode)))
+  (sp-local-pair modes "`" nil :when '(sp-in-string-p sp-in-comment-p))
+  (sp-local-pair modes "'" nil :when '(sp-in-string-p sp-in-comment-p)))
