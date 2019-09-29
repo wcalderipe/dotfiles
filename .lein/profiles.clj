0a2d4da99b5f1d3211501d0aef0af2f6bbb68a21
William Calderipe <wcalderipe@gmail.com>
2019-09-29 10:37:25 +1000

    Remove humane-test-output dependency from project.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,23 +1,17 @@
 {:user {:repl {:dependencies [;; Default Clojure version outside lein projects.
                               ^:displace [org.clojure/clojure "1.10.0"]]}
 
-        :dependencies [
-                       [cider/cider-nrepl "0.21.1"]
+        :dependencies [[cider/cider-nrepl "0.21.1"]
                        [lein-cljfmt "0.6.4"]
                        [mvxcvi/puget "1.1.0"]
-                       [nrepl "0.6.0"]
-                       [pjstadig/humane-test-output "0.9.0"]
-                       ]
+                       [nrepl "0.6.0"]]
 
-        :plugins [
-                  [com.jakemccrary/lein-test-refresh "0.23.0"]
+        :plugins [[com.jakemccrary/lein-test-refresh "0.23.0"]
                   [jonase/eastwood "0.3.5"]
                   [lein-ancient "0.6.15"]
-                  [lein-exec "0.3.7"]
-                  ]
+                  [lein-exec "0.3.7"]]
 
-        :repl-options {
-                       ;; This expression will run when first opening a REPL, in the
+        :repl-options {;; This expression will run when first opening a REPL, in the
                        ;; namespace from :init-ns or :main if specified.
                        :init (do (set! *print-length* 30)
                                  (set! *print-level* 20))
@@ -41,8 +35,5 @@
                                           cider.nrepl/wrap-out
                                           cider.nrepl/wrap-undef
                                           cider.nrepl/wrap-version]}
-
-        :injections [(require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]
         }
  }
