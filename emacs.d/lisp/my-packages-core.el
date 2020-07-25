a22946f4a41a8cdf77caf4a7dcd79e006f26c2f4
William Calderipe <wcalderipe@gmail.com>
2020-07-25 18:28:17 +0200

    Start a major rewrite of my emacs.d

--- emacs.d/lisp/my-packages-core.el
+++ emacs.d/lisp/my-packages-core.el
@@ -1,19 +1,3 @@
-(defun my/install-package-manager ()
-  (let ((bootstrap-file
-         (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-        (bootstrap-version 5))
-    (unless (file-exists-p bootstrap-file)
-      (with-current-buffer
-          (url-retrieve-synchronously
-           "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-           'silent 'inhibit-cookies)
-        (goto-char (point-max))
-        (eval-print-last-sexp)))
-    (load bootstrap-file nil 'nomessage))
-
-  ;; Auto-install `use-package'.
-  (straight-use-package 'use-package))
-
 ;; More convenient key definitions.
 (use-package general
   :straight t
@@ -113,4 +97,4 @@
 
   (evil-mode 1))
 
-(provide 'my-core)
+  (provide 'my-packages-core)
--- emacs.d/lisp/my-vars.el
+++ emacs.d/lisp/my-vars.el
@@ -1,2 +1,18 @@
 (defvar bootstrap-version nil
   "Used by the straight package manager.")
+
+(defconst my/emacs-dir
+  (eval-when-compile (file-truename user-emacs-directory))
+  "The path to the currently loaded .emacs.d directory. Must end with a slash.")
+
+(defconst my/local-dir
+  (concat my/emacs-dir ".local/")
+  "Root directory for local storage. Must end with a slash.")
+
+(defconst my/cache-dir (concat my/local-dir "cache/")
+  "Directory for volatile local storage. Must end with a slash.")
+
+(defconst my/leader "SPC"
+  "Global prefix used in `general' keybindings.")
+
+(provide 'my-vars)
