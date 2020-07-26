96c5f85c2c455dc01e2febe73a4f4637f8e6adc4
William Calderipe <wcalderipe@gmail.com>
2020-07-26 14:28:11 +0200

    Move load theme fn to doom-modeline

--- emacs.d/lisp/my-editor.el
+++ emacs.d/lisp/my-editor.el
@@ -12,22 +12,24 @@
   :hook (after-init . my/init-load-theme)
 
   :preface
+  ;; I'm not happy on where custom-set-faces are being set inside of
+  ;; doom-modeline.
   (defun my/init-load-theme ()
+    (custom-set-faces
+     `(ivy-current-match              ((t :background ,my/color-gray :foreground nil :underline unspecified :weight unspecified)))
+     `(ivy-highlight-face             ((t :background nil :foreground nil :underline unspecified :weight unspecified)))
+     `(ivy-minibuffer-match-face-1    ((t :background nil :inherit bold)))
+     `(ivy-minibuffer-match-face-2    ((t :background nil :foreground ,my/color-cyan :underline t)))
+     `(ivy-minibuffer-match-face-3    ((t :background nil :foreground ,my/color-cyan :underline t)))
+     `(ivy-minibuffer-match-face-4    ((t :background nil :foreground ,my/color-cyan :underline t)))
+     `(ivy-minibuffer-match-highlight ((t :background ,my/color-gray :foreground nil :underline unspecified :weight unspecified)))
+     `(ivy-subdir                     ((t :background nil :underline unspecified :weight unspecified))))
+
     (doom-modeline-mode +1)
     (load-theme 'zenburn t nil))
 
   :config
   (doom-modeline-mode 1))
 
-(defun my/load-editor ()
-  (custom-set-faces
-   `(ivy-current-match              ((t :background ,my/color-gray :foreground nil :underline unspecified :weight unspecified)))
-   `(ivy-highlight-face             ((t :background nil :foreground nil :underline unspecified :weight unspecified)))
-   `(ivy-minibuffer-match-face-1    ((t :background nil :inherit bold)))
-   `(ivy-minibuffer-match-face-2    ((t :background nil :foreground ,my/color-cyan :underline t)))
-   `(ivy-minibuffer-match-face-3    ((t :background nil :foreground ,my/color-cyan :underline t)))
-   `(ivy-minibuffer-match-face-4    ((t :background nil :foreground ,my/color-cyan :underline t)))
-   `(ivy-minibuffer-match-highlight ((t :background ,my/color-gray :foreground nil :underline unspecified :weight unspecified)))
-   `(ivy-subdir                     ((t :background nil :underline unspecified :weight unspecified)))))
 
 (provide 'my-editor)
