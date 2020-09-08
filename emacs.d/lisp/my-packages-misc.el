aa589163615ab27cf586f912c72a0156295c1a73
William Calderipe <wcalderipe@gmail.com>
2020-09-08 08:17:57 +0200

    Add ParEdit Emacs package for Lisp major modes

--- emacs.d/lisp/my-packages-misc.el
+++ emacs.d/lisp/my-packages-misc.el
@@ -139,6 +139,29 @@
                   clojurescript-mode
                   cider-repl-mode))
     (let ((hook (intern (concat (symbol-name mode) "-hook"))))
-      (add-hook hook #'smartparens-mode))))
+      (add-hook hook #'smartparens-mode)))
+
+  ;; Adds to IELM outside the dolist because it doesn't have a mode function.
+  (add-hook 'ielm-mode-hook #'smartparens-mode))
+
+
+;; Minor mode for performing structured editing of S-expression data.
+(use-package paredit
+  :straight t
+  :init
+  ;; Only hooks with Lisp family major modes.
+  (dolist (mode '(emacs-lisp-mode
+                  common-lisp-mode
+                  lisp-mode
+                  clojure-mode
+                  clojurec-mode
+                  clojurescript-mode
+                  cider-repl-mode))
+    (let ((hook (intern (concat (symbol-name mode) "-hook"))))
+      (add-hook hook #'enable-paredit-mode)))
+
+  ;; Adds to IELM outside the dolist because it doesn't have a mode function.
+  (add-hook 'ielm-mode-hook #'enable-paredit-mode))
+
 
 (provide 'my-packages-misc)
