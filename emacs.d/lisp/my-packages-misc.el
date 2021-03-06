69f502ea20d479b4b8f5e8341028a691a5727a4d
William Calderipe <wcalderipe@gmail.com>
2021-03-06 10:11:54 +0100

    Add emacs agressive-indent package for the lisp family

--- emacs.d/lisp/my-packages-misc.el
+++ emacs.d/lisp/my-packages-misc.el
@@ -193,6 +193,21 @@
    "C-M-RET" #'yafolding-toggle-all
    "C-RET" #'yafolding-toggle-element))
 
+
+(use-package aggressive-indent
+  :straight t
+  :defer t
+  :init
+  (dolist (mode '(emacs-lisp-mode
+                  common-lisp-mode
+                  lisp-mode
+                  clojure-mode
+                  clojurec-mode
+                  clojurescript-mode
+                  cider-repl-mode))
+    (let ((hook (intern (concat (symbol-name mode) "-hook"))))
+      (add-hook hook #'aggressive-indent-mode))))
+
 (provide 'my-packages-misc)
 
 ;;; my-packages-misc ends here.
