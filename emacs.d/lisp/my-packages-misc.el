76447887981ba144ed1e646a69343a4b4cea68fe
William Calderipe <wcalderipe@gmail.com>
2021-03-26 11:15:23 +0100

    Constrain exec-path-from-shell execution and config
    Constain the exec-path-from-shell execution only to MacOS.
    
    Set exec-path-from-shell-variables to restrict the access that Emacs has on my
    environment.
    
    Set the JAVA_HOME variable in the exec-path-from-shell-variables to fix an error
    raised by cider.

--- emacs.d/lisp/my-packages-misc.el
+++ emacs.d/lisp/my-packages-misc.el
@@ -212,8 +212,14 @@
 ;; Ensure environment variables inside Emacs look the same as in the user's
 ;; shell.
 (use-package exec-path-from-shell
-  :straight t)
+  :straight t
+  :if (my/macos?)
+  :config
+  ;; Set JAVA_HOME to fix an error raised by cider during the execution of
+  ;; cider-jack-in-clj(s).
+  (setq exec-path-from-shell-variables '("PATH" "MANPATH" "JAVA_HOME"))
 
+  (exec-path-from-shell-initialize))
 
 (provide 'my-packages-misc)
 
