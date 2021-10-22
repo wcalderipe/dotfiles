4f93c9d17d4d6213f03ed338bbcfabe521993eeb
William Calderipe <wcalderipe@gmail.com>
2021-10-22 08:08:47 +0200

    Improve org-roam templates and bindings
    Add the day of the week in the daily template title.
    
    Add two psychological questions in the daily template to track mood change.
    
    Add one question in the daily template to track important work to be done.
    
    Improve work template by adding a headling equals to the file title.
    
    Add bindings to easily navigate on org-roam today, tomorrow, yesterday and find
    a date.

--- emacs.d/lisp/my-packages-core.el
+++ emacs.d/lisp/my-packages-core.el
@@ -138,14 +138,15 @@
   :init
   ;; Custom capture template with my `setup.org' adapted from
   ;; https://www.orgroam.com/manual/Template-Walkthrough.html#Template-Walkthrough
-  (defun my/org-roam--capture-templates ()
+  (defun my/org-roam-capture-templates ()
     "Create the default org-roam capture template with a custom head."
     '(("d" "Default" plain #'org-roam-capture--get-point
        "%?"
        :file-name "%<%Y%m%d%H%M%S>-${slug}"
        :unnarrowed t
        :head "#+setupfile: ~/dev/dotfiles/setup.org
-#+title: ${title}\n")
+#+title: ${title}\n
+* ${title}\n\n")
 
       ("r" "Reading list" plain #'org-roam-capture--get-point
        "%?"
@@ -155,9 +156,9 @@
 - tags :: [[file:reading-list.org][Reading list]]\n
 * ${title}
 :PROPERTIES:
-:STATUS: unread
-:URL: nil
 :CREATED_AT: %t
+:URL: nil
+:STATUS: unread
 :RATE: 0
 :END:\n")
 
@@ -173,19 +174,37 @@
        :file-name "%<%Y%m%d%H%M%S>-${slug}"
        :head "#+setupfile: ~/dev/dotfiles/setup.org
 #+title: ${title}\n
-- tags :: [[file:20210311170624-multis.org][Multis]]\n")))
+- tags :: [[file:20210311170624-multis.org][Multis]]\n
+* ${title}\n\n")))
+
+  (defun my/org-roam-dailies-capture-templates ()
+    '(("d" "Default" entry
+       #'org-roam-capture--get-point
+       "* %?"
+       :file-name "daily/%<%Y-%m-%d>"
+       ;; Format the title with the date and the day of the week.
+       ;; See https://www.gnu.org/software/emacs/manual/html_node/elisp/Time-Parsing.html
+       :head "#+title: %<%Y-%m-%d (%A)>\n
+* Today\n
+** What am I worried about?\n
+** What am I grateful for?\n
+** What would make my day great?\n")))
 
   (setq org-roam-directory my/org-dir
-        org-roam-db-location (concat my/org-dir ".config/org-roam.db"))
-
-  (setq org-roam-capture-templates (my/org-roam--capture-templates))
+        org-roam-db-location (concat my/org-dir ".config/org-roam.db")
+        org-roam-capture-templates (my/org-roam-capture-templates)
+        org-roam-dailies-capture-templates (my/org-roam-dailies-capture-templates))
 
   :config
   (general-define-key
    :keymaps 'org-roam-mode-map
    "C-c n l" #'org-roam
    "C-c n f" #'org-roam-find-file
-   "C-c n g" #'org-roam-graph-show)
+   "C-c n g" #'org-roam-graph-show
+   "C-c n t" #'org-roam-dailies-today
+   "C-c n T" #'org-roam-dailies-tomorrow
+   "C-c n y" #'org-roam-dailies-yesterday
+   "C-c n d" #'org-roam-dailies-find-date)
 
   (general-define-key
    :keymaps 'org-mode-map
