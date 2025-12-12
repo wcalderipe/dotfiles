89fe8151df1a2b34beca82539df2759fb1e35628
William Calderipe <wcalderipe@gmail.com>
2025-12-12 14:16:37 +0100

    Change neogit open kind to vsplit instead of tab

--- nvim/lua/custom/plugins/neogit.lua
+++ nvim/lua/custom/plugins/neogit.lua
@@ -4,14 +4,26 @@ return {
   'NeogitOrg/neogit',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'sindrets/diffview.nvim',
     'nvim-telescope/telescope.nvim',
   },
   config = function()
     local neogit = require 'neogit'
 
     neogit.setup {
-      kind = 'tab',
+      -- If neogit is on `tab` mode, when searching and navigating to a buffer
+      -- using telescope it'll open in the current tab which is the expected
+      -- behavior but an annoying one because it never closes them. Using
+      -- `replace` stacks the buffer on top and buffer navigation will just
+      -- work.
+      kind = 'vsplit',
+      mappings = {
+        status = {
+          -- Disable closing on "q" in the status buffer because it often
+          -- triggers the record macro and it's annoying. I want ONE way to
+          -- close buffers.
+          ['q'] = false,
+        },
+      },
     }
 
     vim.keymap.set('n', '<leader>gs', function()
