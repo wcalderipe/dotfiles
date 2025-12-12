55ddc3b8a945864019e03b891f2a47d81bb039d2
William Calderipe <wcalderipe@gmail.com>
2025-12-12 14:17:16 +0100

    Update git-conflict plugin

--- nvim/lua/custom/plugins/git-conflict.lua
+++ nvim/lua/custom/plugins/git-conflict.lua
@@ -2,5 +2,16 @@
 return {
   'akinsho/git-conflict.nvim',
   version = '*',
-  config = true,
+  config = function()
+    require('git-conflict').setup {
+      default_mappings = true,
+      default_commands = true,
+      disable_diagnostics = false,
+      list_opener = 'copen', -- command or function to open the conflicts list
+      highlights = { -- They must have background color, otherwise the default color will be used
+        incoming = 'DiffAdd',
+        current = 'DiffText',
+      },
+    }
+  end,
 }
